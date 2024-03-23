b53037b4a9e552adb8773eb9812de975db2f3e15
Shafqat Abbas <chshafqat4422@gmail.com>
2024-03-23 23:54:17 +0500

    Make Adjustments

--- src/components/Images/imageCard.js
+++ src/components/Images/imageCard.js
@@ -1,27 +1,21 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faUser, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
-import styles from './images.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+import styles from "./images.module.css";
 
-const imageCard = ({images}) => {
+const imageCard = ({ images }) => {
   return (
     <div className={styles.card}>
-      <img
-        src={images.url}
-        alt="imageCard"
-      />
+      <img src={images.url} alt="imageCard" />
       <h1>{images.name}</h1>
-      <p>
-        {images.desc}
-      
-      </p>
+      <p>{images.desc}</p>
       <div>
         <p>
-        <FontAwesomeIcon icon={faUser} style={{ color: 'blue' }} />
+          <FontAwesomeIcon icon={faUser} style={{ color: "blue" }} />
           <span>{images.author}</span>
         </p>
         <p>
-        <FontAwesomeIcon icon={faCalendarAlt} style={{ color: 'blue' }} />
+          <FontAwesomeIcon icon={faCalendarAlt} style={{ color: "blue" }} />
           <span>{images.date}</span>
         </p>
       </div>
